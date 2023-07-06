a878cde8f144f295cde9c25ca1b23554ac10b0bb
Henri-Michel Thébaud <hmthebaud@trusted-objects.com>
2023-07-06 18:22:41 +0200

    Update to-protect

--- to-protect/Files4Flashing/Flash-TOProtect-and-Storage.sh
+++ to-protect/Files4Flashing/Flash-TOProtect-and-Storage.sh
@@ -4,7 +4,7 @@
 # STM32_Programmer_CLI -c port=SWD -d PoC_generic_tls_wifi_TOP.bin 0x08000000 --verify
 
 # Flash TO-Protect to 0x860.0000
-STM32_Programmer_CLI -c port=SWD -d TO-Protect-eval.cortexm3.bin 0x08060000 --verify
+STM32_Programmer_CLI -c port=SWD -d TO-Protect-prod.cortexm3.bin 0x08060000 --verify
 
 # Flash its Secure Storage to 0x80A.0000
 STM32_Programmer_CLI -c port=SWD -d SecureStorage_1.bin 0x080A0000 --verify
